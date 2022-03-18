2729a087e5938fa2f977d0a804d9111ef1772e58
aldaalbarran <aldaxalbarran@gmail.com>
2022-03-17 21:05:11 -0600

    Calculadora de medidas y precios de cortes en MDF (carpintería)

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$("#btnCalcular").click(function () {
+    if ($("#hPlaca").val() == '' || $("#wPlaca").val() == '') {
+        $("#error").removeClass("d-none");
+        $("#error").html("<strong>ERROR</strong> Introducir las medidas de la placa.");
+    } else if ($("#hPieza").val() == '' || $("#wPieza").val() == '') {
+        $("#error").removeClass("d-none");
+        $("#error").html("<strong>ERROR</strong> Introducir las medidas de la pieza.");
+    } else if ($("#precioPlaca").val() == '') {
+        $("#error").removeClass("d-none");
+        $("#error").html("<strong>ERROR</strong> Introducir el precio de la placa.");
+    } else if ($("#pzasFigura").val() == '') {
+        $("#error").removeClass("d-none");
+        $("#error").html("<strong>ERROR</strong> Introducir el número de piezas por figura.");
+    } else {
+        $("#error").addClass("d-none");
+        $("#results").removeClass("d-none");
+
+        var hPlaca = $("#hPlaca").val(); //altura de la placa
+        var wPlaca = $("#wPlaca").val(); //ancho de lap placa
+        var aPlaca = area(hPlaca, wPlaca); //area de la placa
+        $("#aPlaca").val(aPlaca); //imprimir área de la placa
+
+        var hPieza = $("#hPieza").val(); //altura de la pieza
+        var wPieza = $("#wPieza").val(); //ancho de la pieza
+        var aPieza = area(hPieza, wPieza); //area de la pieza
+        $("#aPieza").val(aPieza); //imprimir área de la pieza
+
+        var pzasPlaca = Math.floor(division(aPlaca, aPieza)); //calcular número de piezas por placa
+        $("#pzasPlaca").val(pzasPlaca); //imprimir número de piezas por placa
+
+        var precioPlaca = $("#precioPlaca").val(); //precio de la placa
+        var precioPieza = division(precioPlaca, pzasPlaca); //precio de la pieza
+        $("#precioPieza").val(round(precioPieza, 2)); //imprimir preico de la pieza
+
+        var pzasFigura = $("#pzasFigura").val(); //piezas por figura
+        var precioFigura = pzasFigura * precioPieza; //precio por figura
+        $("#precioFigura").val(round(precioFigura, 2)); //imprimir precios por figura
+    }
+});
+
+$( "#btnBorrar" ).click(function() {
+    $("#hPieza").val("");
+    $('#wPieza').val("");
+    $('#precioPlaca').val("");
+    $('#pzasFigura').val("");
+    
+    $("#results").addClass("d-none");
+});
+
+function area(largo, ancho) {
+    var area = largo * ancho;
+    return area;
+}
+
+function division(dividendo, divisor) {
+    var division = dividendo / divisor;
+    return division;
+}
+
+const round = (number, decimalPlaces) => {
+    const factorOfTen = Math.pow(10, decimalPlaces);
+    return Math.round(number * factorOfTen) / factorOfTen;
+}
+
